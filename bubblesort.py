dc179a644ce04817be881c00ac023c480c9a0dbf
Yash-develope <134491759+Yash-develope@users.noreply.github.com>
2024-02-12 22:45:04 +0530

    bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -8,6 +8,7 @@ for i in range(lim):
     arr.append(inp)
 
 print(arr)
+# This is for ascending order let us if the array=[23,1,45,32,67] then the output will be the array=[1,23,32,45,67]
 def bbblesort(arr):
     n=len(arr)
     for i in range(n-1):
@@ -18,3 +19,16 @@ def bbblesort(arr):
     print(f'So the sorted array will be :{arr}')
 
 bbblesort(arr)
+#This is for descending sort let us if the array will be =[23,1,45,32,67] then the output will be the array=[67,45,32,23,1]
+
+def descendingbbblesort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+        print(f'{i+1}st pass will be after completion :{arr}')
+    print(f'So the sorted array will be :{arr}')
+
+descnedingbbblesort(arr)
+
