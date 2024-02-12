f33e945d2ac37e3fe2defc19ba744d48f5b045ec
Yash-develope <134491759+Yash-develope@users.noreply.github.com>
2024-02-12 22:45:46 +0530

    bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -25,7 +25,7 @@ def descendingbbblesort(arr):
     n=len(arr)
     for i in range(n-1):
         for j in range(0,n-i-1):
-            if arr[j]>arr[j+1]:
+            if arr[j]<arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
         print(f'{i+1}st pass will be after completion :{arr}')
     print(f'So the sorted array will be :{arr}')
